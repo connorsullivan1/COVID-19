7927042edcfc18d3121d041d2286e16ef8e8ce35
Connor Sullivan <sulliamr@bc.edu>
2020-04-27 13:42:45 -0400

    Complete

---
+++ Covid-19/NewsTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NewsTableViewCell.swift
+//  Covid-19
+//
+//  Created by Connor on 4/15/20.
+//  Copyright © 2020 Connor Sullivan. All rights reserved.
+//
+
+import UIKit
+import SkeletonView
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var publisherLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var cellView: UIView!
+    
+    
+}
